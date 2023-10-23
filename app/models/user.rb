56167c54bcfecb479e77f199f8a427a1da60933c
kerlosbahaa3 <aboelking4@gmail.com>
2023-10-23 19:51:33 +0300

    food-list

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,3 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
-
   has_many :recipes, foreign_key: :author_id
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :recipes
   resources :foods
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "recipes#all"
+  root "recipes#index"
   get '/shoppings', to: 'shoppings#index', as: 'shoppings'
-  get '/all', to: 'recipes#all', as: 'public_recipes'
 end
