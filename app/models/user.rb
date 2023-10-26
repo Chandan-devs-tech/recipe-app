e637d0967a8513e19cd8182ad0ca01945443e81f
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-10-27 00:48:16 +0530

    Update app/models/user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, presence: true
+
+  has_many :foods, foreign_key: 'user_id'
 end
