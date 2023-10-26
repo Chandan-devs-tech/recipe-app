4b17c97ac3177658bec7e4d8ee803de3d82f0c84
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-10-27 00:47:57 +0530

    Update app/models/food.rb

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,7 @@
 class Food < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, foreign_key: 'user_id', class_name: 'User'
+  validates :name, presence: true
+  validates :measurement_unit, presence: true
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
