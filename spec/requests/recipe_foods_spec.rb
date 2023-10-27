52059d0aa2f64c851266d16b642ad2c1073bb9ef
Chandan-devs-tech <chandanguptabsb@gmail.com>
2023-10-28 01:48:20 +0530

    Fix linters issues

--- spec/requests/recipe_foods_spec.rb
+++ spec/requests/recipe_foods_spec.rb
@@ -76,7 +76,6 @@ RSpec.describe '/recipe_foods', type: :request do
         end.to change(RecipeFood, :count).by(0)
       end
 
-
       it "renders a response with 422 status (i.e. to display the 'new' template)" do
         post recipe_foods_url, params: { recipe_food: invalid_attributes }
         expect(response).to have_http_status(:unprocessable_entity)
